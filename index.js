5f7d98bdb43aa811cd5d140dd733951ead6b2bdf
BreMea <63671187+BreMea@users.noreply.github.com>
2020-09-13 13:04:00 -0400

    Fix Formatting

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ scratch.UserSession.create('BGMead', '', function (err, user) {
                 weather.find({search: decode(value), degreeType: 'F'}, function(err, result) {
                     if(err) console.log(err);
                     if (result !== undefined) {
-						console.log('Got data for ' + decode(value));
-						cloud.set('☁ time', new Date().addHours(result[0].location.timezone).getHours() + 4);
-						cloud.set('☁ temp', result[0].current.temperature + '00' + result[0].current.skycode + '00' + result[0].current.feelslike + '00');
-						cloud.set('☁ city', 0);
-						cloud.set('☁ status', encode(result[0].current.skytext));
-						cloud.set('☁ forecast1', encode(result[0].forecast[0].shortday) + '00' + result[0].forecast[0].low + '00' + result[0].forecast[0].high + '00' + result[0].forecast[0].skycodeday + '00');
-						cloud.set('☁ forecast2', encode(result[0].forecast[1].shortday) + '00' + result[0].forecast[1].low + '00' + result[0].forecast[1].high + '00' + result[0].forecast[1].skycodeday + '00');
-						cloud.set('☁ forecast3', encode(result[0].forecast[2].shortday) + '00' + result[0].forecast[2].low + '00' + result[0].forecast[2].high + '00' + result[0].forecast[2].skycodeday + '00');
-						cloud.set('☁ forecast4', encode(result[0].forecast[3].shortday) + '00' + result[0].forecast[3].low + '00' + result[0].forecast[3].high + '00' + result[0].forecast[3].skycodeday + '00');
-						cloud.set('☁ wind', encode(result[0].current.winddisplay));
-						cloud.set('☁ humidity', result[0].current.humidity);
-					} else {
-						cloud.set('☁ city', -1);
-					}
+                        console.log('Got data for ' + decode(value));
+                        cloud.set('☁ time', new Date().addHours(result[0].location.timezone).getHours() + 4);
+                        cloud.set('☁ temp', result[0].current.temperature + '00' + result[0].current.skycode + '00' + result[0].current.feelslike + '00');
+                        cloud.set('☁ city', 0);
+                        cloud.set('☁ status', encode(result[0].current.skytext));
+                        cloud.set('☁ forecast1', encode(result[0].forecast[0].shortday) + '00' + result[0].forecast[0].low + '00' + result[0].forecast[0].high + '00' + result[0].forecast[0].skycodeday + '00');
+                        cloud.set('☁ forecast2', encode(result[0].forecast[1].shortday) + '00' + result[0].forecast[1].low + '00' + result[0].forecast[1].high + '00' + result[0].forecast[1].skycodeday + '00');
+                        cloud.set('☁ forecast3', encode(result[0].forecast[2].shortday) + '00' + result[0].forecast[2].low + '00' + result[0].forecast[2].high + '00' + result[0].forecast[2].skycodeday + '00');
+                        cloud.set('☁ forecast4', encode(result[0].forecast[3].shortday) + '00' + result[0].forecast[3].low + '00' + result[0].forecast[3].high + '00' + result[0].forecast[3].skycodeday + '00');
+                        cloud.set('☁ wind', encode(result[0].current.winddisplay));
+                        cloud.set('☁ humidity', result[0].current.humidity);
+                    } else {
+                        cloud.set('☁ city', -1);
+                    }
                 });
             }
         });
